580f3a48bb11b89135a58c8c0208c623ed128ec4
chromatic-aberration <ashaxyn@vivaldi.net>
2024-12-31 18:03:30 +0100

    upd buildscript

--- launcher/build.sh
+++ launcher/build.sh
@@ -1,3 +1,3 @@
-source .\linvenv\Scripts\activate
+source ./venv/bin/activate
 
-python -m nuitka --standalone --onefile --enable-plugin=no-qt --enable-plugin=tk-inter --linux-onefile-icon=launcher.png --include-data-dir=assets=. launcher.py
+python -m nuitka --standalone --onefile --enable-plugin=no-qt --enable-plugin=tk-inter --linux-onefile-icon=launcher.png --include-data-dir=assets=. --output-dir=.. --output-filename=launcher-lin launcher.py
